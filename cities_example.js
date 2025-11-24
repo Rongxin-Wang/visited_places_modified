fd3c78aadca178140e1eeb4d93a09fd240edc40b
Rongxin-Wang <ronwa@dtu.dk>
2025-11-24 18:21:30 +0100

    Replaced cities.js and countries.js with example files to protect privacy

---
+++ cities_example.js
@@ -0,0 +1,15 @@
+// Delete the '_example' in the file name before editting
+// fillKey = city_lived (> 3 month), city_stayed (> 1 week), city_visited, city_passed_through, city_planning
+CITIES =
+[
+   // Lived
+   {name: 'Beijing', latitude: 39.9042, longitude: 116.4074, radius: 3, fillKey: 'city_lived', date: ''},
+   // Stayed
+   {name: 'Paris', latitude: 48.8566, longitude: 2.3522, radius: 3, fillKey: 'city_lived', date: ''},
+   // Visited
+   {name: 'Tokyo', latitude: 35.6762, longitude: 139.6503, radius: 3, fillKey: 'city_visited', date: ''},
+   // Passed through
+   {name: 'Seoul', latitude: 37.5665, longitude: 126.9780, radius: 3, fillKey: 'city_passed_through', date: ''},
+   // Planning
+   {name: 'Munich', latitude: 48.1351, longitude: 11.5820, radius: 3, fillKey: 'city_planning', date: ''},
+]
---
+++ countries_example.js
@@ -0,0 +1,8 @@
+// Use ISO alpha-3 country codes, see e.g. https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
+// Delete the '_example' in the file name before editting
+// fillKey = country_visited (Green), country_planning (Yellow)
+COUNTRIES =
+{
+    CHN: {fillKey: 'country_visited'},   // China
+    GBR: {fillKey: 'country_planning'},   // United Kingdom
+}
